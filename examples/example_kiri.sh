536a9a63c22db8116499a3ceb74a8d237b07eddf
Torsten Wylegala <mail@twyleg.de>
2023-12-20 15:54:50 +0100

    Added subfolder docker/, added run and build scripts, minor changes

--- examples/example_kiri.sh
+++ examples/example_kiri.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source $SCRIPT_DIR/setup_example_project.sh example_kiri
 
-podman build --tag kicad_toolchain $SCRIPT_DIR/../
-
-podman run -it -v $SCRIPT_DIR/data/example_kiri/:/root/project -w /root/project -p 8282:8282 kicad_toolchain \
+podman_build
+podman_run -p 8282:8282 kicad_toolchain \
 	kiri -p 8282 -r
--- examples/setup_example_project.sh
+++ examples/setup_example_project.sh
@@ -2,17 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-function setup_python () {
-  python -m venv venv
-  source venv/bin/activate
-  pip install $SCRIPT_DIR/..
-}
-
 function setup_template_project_kicad () {
-  if [ ! -d "$SCRIPT_DIR/data/template_project_kicad" ]; then
-    git clone https://github.com/twyleg/template_project_kicad.git $SCRIPT_DIR/data/template_project_kicad
+  if [ ! -d "$SCRIPT_DIR/.data/template_project_kicad" ]; then
+    git clone https://github.com/twyleg/template_project_kicad.git $SCRIPT_DIR/.data/template_project_kicad
   else
-    cd $SCRIPT_DIR/data/template_project_kicad
+    cd $SCRIPT_DIR/.data/template_project_kicad
     if [ ! -z "$(git status --porcelain)" ]; then
       echo -e "\033[0;31;5m-- Caution: Changes in template_project_kicad --\033[0;0m"
     else
@@ -21,15 +15,22 @@ function setup_template_project_kicad () {
   fi
 }
 
+function podman_build () {
+	podman build --tag kicad_toolchain $SCRIPT_DIR/../docker
+}
+
+function podman_run () {
+	podman run -it -v $EXAMPLE_DIR/:/root/project -w /root/project $*
+}
+
 EXAMPLE_NAME=$1
 
-TEMPLATE_DIR=$SCRIPT_DIR/data/template_project_kicad
-EXAMPLE_DIR=$SCRIPT_DIR/data/$EXAMPLE_NAME
+TEMPLATE_DIR=$SCRIPT_DIR/.data/template_project_kicad
+EXAMPLE_DIR=$SCRIPT_DIR/.data/$EXAMPLE_NAME
 
-mkdir -p $SCRIPT_DIR/data/
+mkdir -p $SCRIPT_DIR/.data/
 rm -rf $EXAMPLE_DIR
 
-# setup_python
 setup_template_project_kicad
 
 cp -r $TEMPLATE_DIR $EXAMPLE_DIR
